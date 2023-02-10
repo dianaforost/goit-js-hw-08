9ea56a704b40d0a75f5d9fff5cab765f61e4bf0c
Diana Forostiana <dianaforost@gmail.com>
2023-02-10 09:20:59 +0100

    Update 01-gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,29 @@
 // Add imports above this line
-import SimpleLightbox from '../../node_modules/simplelightbox';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const gallery = document.querySelector('.gallery');
 
+function createGalleryMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+`;
+    })
+    .join('');
+}
+gallery.insertAdjacentHTML('afterbegin', createGalleryMarkup(galleryItems));
+new SimpleLightbox('.gallery .gallery__link', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  captionPosition: 'bottom',
+});
 console.log(galleryItems);

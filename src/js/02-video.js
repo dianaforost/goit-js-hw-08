3530b624d8493f82694cb4dc93bc087d896c1f60
Diana Forostiana <dianaforost@gmail.com>
2023-02-11 20:06:06 +0100

    Update 02-video
    And added vimeo + lodash

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,9 @@
-
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+function onPlay(data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+}
+player.setCurrentTime(Number(localStorage.getItem('videoplayer-current-time')));
+player.on('timeupdate', throttle(onPlay, 1000));

84a9abae5207f6297c194b6ed4a8c921c4d5c41c
Diana Forostiana <dianaforost@gmail.com>
2023-02-12 17:54:39 +0100

    Update 03-feedback

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,33 @@
+import throttle from 'lodash.throttle';
+const form = document.querySelector('.feedback-form');
+const email = document.querySelector('.feedback-form input');
+const message = document.querySelector('.feedback-form textarea');
+let formData = {};
 
+function valueInForm(event) {
+  formData[event.target.name] = event.target.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+  console.log(formData);
+}
+
+function submitInForm(event) {
+  event.preventDefault();
+  event.target.reset();
+  console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+  localStorage.removeItem('feedback-form-state');
+}
+
+function updateOutput() {
+  const formState = JSON.parse(localStorage.getItem('feedback-form-state'));
+  if (formState) {
+    email.value = formState.email;
+    message.value = formState.message;
+  } else {
+    email.value = '';
+    message.value = '';
+  }
+}
+updateOutput();
+
+form.addEventListener('input', throttle(valueInForm, 500));
+form.addEventListener('submit', submitInForm);
